f193d753fe1b5263a313f96a340c48d01d1d0df7
何志坚 <1305696249@qq.com>
2022-02-26 16:03:47 +0800

    创建generator组件库

---
+++ projects/generator/src/lib/generator.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'h-generator',
+  template: `
+    <p>
+      generator works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class GeneratorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/generator/src/lib/generator.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { GeneratorComponent } from './generator.component';
+
+
+
+@NgModule({
+  declarations: [
+    GeneratorComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    GeneratorComponent
+  ]
+})
+export class GeneratorModule { }
