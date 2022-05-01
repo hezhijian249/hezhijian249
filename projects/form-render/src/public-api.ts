3b9f64fcd448ba24b48a24c67204d01f93db08a0
何志坚 <1305696249@qq.com>
2022-05-01 23:37:32 +0800

    对外暴露组件和枚举

--- projects/form-render/src/public-api.ts
+++ projects/form-render/src/public-api.ts
@@ -5,3 +5,7 @@
 export * from './lib/form-render.service';
 export * from './lib/form-render.component';
 export * from './lib/form-render.module';
+
+export * from './lib/components/form/form.component'
+
+export * from './lib/enum/FormFieldEnum'
