3505ab95bf163af2e2a90295325d15d9675e3932
何志坚 <1305696249@qq.com>
2022-05-01 23:36:35 +0800

    form组件类型枚举

---
+++ projects/form-render/src/lib/enum/FormFieldEnum.ts
@@ -0,0 +1,17 @@
+/**
+ * 表单字段枚举
+ */
+export enum FormFieldEnum {
+  /**
+   * 输入框
+   */
+  INPUT = 'InputComponent',
+  /**
+   * 下拉选择器
+   */
+  SELECT = 'SelectComponent',
+  /**
+   * 树形选择控件
+   */
+  TREE_SELECT = 'TreeSelectComponent'
+}
