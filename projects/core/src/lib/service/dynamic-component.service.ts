a2dc9018cdf4eb3dce84485b4140e67522c42e1a
何志坚 <1305696249@qq.com>
2022-05-01 23:38:24 +0800

    动态组件service，用户注册和查询

---
+++ projects/core/src/lib/service/dynamic-component.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DynamicComponentService {
+
+  private component: { [key: string]: any } = {}
+
+  /**
+   * 注册动态组件
+   * @param component
+   */
+  registerComponent(component: any[] | any) {
+    if (Array.isArray(component)) {
+      component.forEach((item) => {
+        if (item.alias) {
+          this.component[item.alias] = item.component;
+        } else {
+          this.component[item.name] = item;
+        }
+      })
+    } else {
+      this.component[component.name] = component;
+    }
+  }
+
+  /**
+   * 获取组件
+   * @param key 组件的key
+   */
+  getComponent(key: string | any) {
+    if (typeof key === 'object') {
+      return key
+    }
+    return this.component[key] || null
+  }
+}
