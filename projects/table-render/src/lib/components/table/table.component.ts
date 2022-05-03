e538a2f33d03fc4b598f4e7979c44ef44977b275
何志坚 <1305696249@qq.com>
2022-05-03 18:03:27 +0800

    table组件基础框架搭建

---
+++ projects/table-render/src/lib/components/table/table.component.ts
@@ -0,0 +1,44 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { BaseColumn } from "../../entity/BaseColumn";
+import { TableConfig } from "../../entity/TableConfig";
+
+@Component({
+  selector: 'h-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.scss']
+})
+export class TableComponent implements OnInit {
+
+  /**
+   *  配置信息
+   */
+  @Input()
+  config: TableConfig | undefined;
+
+  /**
+   * 表格列
+   */
+  @Input()
+  columns: BaseColumn[] = []
+
+  @Output()
+  cellClick: EventEmitter<any> = new EventEmitter<any>();
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  getConfig(column: BaseColumn, data: any): BaseColumn {
+    return { ...column, value: data[column.key] };
+  }
+
+  clickHandle(data: any, rowData: any, config: BaseColumn) {
+    this.cellClick.emit({
+      ...data,
+      rowData: rowData,
+      config: config
+    })
+  }
+}
