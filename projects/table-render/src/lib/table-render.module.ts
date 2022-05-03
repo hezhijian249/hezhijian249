0f0c3b752d3e6193736f9c0469bc5e2c7f47e04d
何志坚 <1305696249@qq.com>
2022-05-03 18:05:51 +0800

    对外暴露table组件和自动注册动态组件

--- projects/table-render/src/lib/table-render.module.ts
+++ projects/table-render/src/lib/table-render.module.ts
@@ -1,16 +1,41 @@
 import { NgModule } from '@angular/core';
 import { TableRenderComponent } from './table-render.component';
+import { TableComponent } from './components/table/table.component';
+import { NzTableModule } from "ng-zorro-antd/table";
+import { CommonModule } from "@angular/common";
+import { CoreModule, DynamicComponentService } from "core";
+import { TextComponent } from './components/cell/text/text.component';
+import { OperatorComponent } from './components/cell/operator/operator.component';
 
+const NZ_MODULE = [
+  NzTableModule
+]
 
+const DYNAMIC_COMPONENT = [
+  TextComponent,
+  OperatorComponent
+]
 
 @NgModule({
   declarations: [
-    TableRenderComponent
+    TableRenderComponent,
+    TableComponent,
+    TextComponent,
+    OperatorComponent
   ],
   imports: [
+    CommonModule,
+    CoreModule,
+    ...NZ_MODULE,
   ],
   exports: [
-    TableRenderComponent
+    TableRenderComponent,
+    TableComponent
   ]
 })
-export class TableRenderModule { }
+export class TableRenderModule {
+
+  constructor(private dynamicComponentService: DynamicComponentService) {
+    dynamicComponentService.registerComponent(DYNAMIC_COMPONENT)
+  }
+}
--- projects/table-render/src/public-api.ts
+++ projects/table-render/src/public-api.ts
@@ -5,3 +5,5 @@
 export * from './lib/table-render.service';
 export * from './lib/table-render.component';
 export * from './lib/table-render.module';
+
+export * from './lib/entity/TableConfig'
