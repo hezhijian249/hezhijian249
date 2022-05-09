aa6f47c2d54a81b8b0186fa3a8819abee58e96f5
何志坚 <1305696249@qq.com>
2022-05-09 23:45:43 +0800

    select下拉框实体属性添加

--- projects/form-render/src/lib/entity/SelectField.ts
+++ projects/form-render/src/lib/entity/SelectField.ts
@@ -12,4 +12,44 @@ export interface SelectField extends FormField {
    * 标签值的key
    */
   valueKey?: string;
+
+  /**
+   * 请求地址
+   */
+  url?: string;
+
+  /**
+   * 请求方式
+   */
+  method?: 'post' | 'get';
+
+  /**
+   * 请求数据
+   */
+  data?: any;
+
+  /**
+   * 加载状态
+   */
+  loading?: boolean;
+
+  /**
+   * 允许清除
+   */
+  allowClear?: boolean;
+
+  /**
+   * 最多选中多少个标签
+   */
+  maxMultipleCount?: number;
+
+  /**
+   * 最多显示多少个 tag, 默认5个
+   */
+  maxTagCount?: number;
+
+  /**
+   * 设置select 的模式，默认default
+   */
+  mode?: 'multiple' | 'tags' | 'default'
 }
