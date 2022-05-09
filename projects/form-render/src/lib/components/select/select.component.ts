41c7b2ae5a0c6af9114089023fb933d6acdc8cfc
何志坚 <1305696249@qq.com>
2022-05-09 21:54:21 +0800

    添加自动请求远程数据和多选

--- projects/form-render/src/lib/components/select/select.component.ts
+++ projects/form-render/src/lib/components/select/select.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from "@angular/forms";
 import { SelectField } from "../../entity/SelectField";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'h-select',
@@ -15,10 +16,49 @@ export class SelectComponent implements OnInit {
   @Input()
   formGroup: FormGroup | undefined;
 
-  constructor() {
+  maxMultipleCount: number = Infinity;
+
+  constructor(private http: HttpClient) {
   }
 
   ngOnInit(): void {
+    if (this.config && this.config?.url) {
+      switch (this.config?.method) {
+        case 'post':
+          this.config.loading = true;
+          this.http.post<any[]>(this.config?.url, this.config?.data).subscribe(res => {
+            this.config!.options = res;
+          }, (err) => {
+            console.error(err);
+          }, () => {
+            this.config!.loading = false;
+          })
+          break;
+        case 'get':
+          let url = this.config.url;
+          let param = ''
+          if (this.config.data) {
+            Object.keys(this.config.data).forEach(key => {
+              param = `${param}${key}=${this.config?.data[key]}`
+            })
+            if (url.indexOf('?') > -1) {
+              url = `${url}&${param}`
+            } else {
+              url = `${url}?${param}`
+            }
+          }
+          this.config.loading = true;
+          this.http.get<any[]>(url).subscribe(res => {
+            this.config!.options = res;
+          }, (err) => {
+            console.error(err);
+          }, () => {
+            this.config!.loading = false;
+          })
+          break;
+      }
+    }
+
   }
 
 }
